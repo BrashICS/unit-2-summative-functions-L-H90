77c6d68eb7f9f87224880615a63dd9e94f26acc8
ArxelV <landon.holladay@stu.ocsb.ca>
2024-10-22 11:15:55 -0400

    commit

--- main.js
+++ main.js
@@ -28,9 +28,6 @@ function avg(n1, n2) {
     return answer
 }
 
-
-
-
 // Round to the nearest `decimals` number of decimals
 function round(value, decimals) {
     return Math.round(value * 10**decimals) / 10**decimals;
@@ -40,8 +37,24 @@ function round(value, decimals) {
 function round_user(value) {
     // Get the number of decimals from the "rounding" box
     let d = Number(document.getElementById("rounding").value)
-    
+    let answer = Math.round(value * 10**d) / 10**d;
+    return answer
 }
+
+function length(x1, y1, x2, y2) {
+    let answer_1 =(delta(x2, x1)) **2 
+    let answer_2 =(delta(y2, y1)) **2
+    let x = Math.sqrt(answer_1)
+    let y = Math.sqrt(answer_2)
+    let z = `${x}, ${y}`
+    return z
+}
+
+
+
+
+
+
 
 // Calculate the y-value of a parabola from standand form
 function y_quad(a, b, c, x) {
